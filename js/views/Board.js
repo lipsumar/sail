9a62719bbe2e2d06fd9b468a9feb5e653542a5cf
Emmanuel Pire <piremmanuel@gmail.com>
2017-03-30 14:36:21 +0200

    CardFloorplan (unofficial)

--- js/views/Board.js
+++ js/views/Board.js
@@ -5,9 +5,10 @@ var Backbone = require('backbone'),
     CardTimeline = require('./CardTimeline.js'),
     CardCount = require('./CardCount.js'),
     CardScatterPlot = require('./CardScatterPlot.js'),
+    CardFloorplan = require('./CardFloorplan.js'),
     BoardModel = require('../models/Board');
 
-var CardTypes = {Card, CardTimeline, CardCount, CardScatterPlot};
+var CardTypes = {Card, CardTimeline, CardCount, CardScatterPlot, CardFloorplan};
 var editIcon = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100"><g transform="translate(0,-952.36218)"><path style="opacity:1;stroke:none;stroke-width:4;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-opacity:1;stroke-dashoffset:0;marker:none;visibility:visible;display:inline;overflow:visible;enable-background:accumulate;fill-opacity:1;" d="M 70 13 L 61.4375 21.5625 L 78.4375 38.5625 L 87 30 L 70 13 z M 58.59375 24.40625 L 20.4375 62.5625 L 37.4375 79.5625 L 75.59375 41.40625 L 58.59375 24.40625 z M 17.84375 65.65625 L 13 87 L 34.34375 82.15625 L 17.84375 65.65625 z " transform="translate(0,952.36218)"/></g></svg>'
 
 var Board = Backbone.View.extend({
